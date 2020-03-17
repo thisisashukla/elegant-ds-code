9816eaf157ffe4e7a2942d07897eb85a990844e7
thisisashukla <work.ankurshukla@gmail.com>
2020-03-17 15:04:11 +0530

    added numerical and categorical description functions

--- py_scripts/eda.py
+++ py_scripts/eda.py
@@ -9,17 +9,42 @@ class Explore():
 
     def __init__(self, df):
         self.df = df
+        self.num_descr = None
+        self.cat_descr = None
 
-    def describe(self):
+    def numDescribe(self):
         descr = self.df.describe().T
-        na_counts = self.completeness()
-        descr.loc[:, 'na %'] = na_counts/descr['count']
-        
-        self.descr = descr.T
-        print(self.descr)
+        descr.loc[:, 'na%'] = (self.naCount(descr.index)/descr['count'])*100
+        descr.loc[:, 'unique'] = [len(self.df[x].unique()) for x in descr.index]
+        descr.loc[descr['count'] == 0, 'na%'] = np.nan
 
-    def completeness(self):
-        return self.df.isna().sum(axis = 0)
+        cols = ['count', 'na%', 'unique', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
+        self.num_descr = descr[cols].T
+        self.num_cols = self.num_descr.columns.tolist()
+
+        return self.num_descr
+
+    def catDescribe(self):
+        try:
+            assert self.num_descr != None
+        except:
+            print('Warning: numDescribe function needs to be called before catDescribe')
+            self.numDescribe()
+        
+        self.cat_cols = [x for x in self.df.columns if x not in self.num_cols]
+        descr = pd.DataFrame(columns = ['count', 'na%', 'unique'], index = self.cat_cols)
+        descr.loc[:, 'count'] = self.df[self.cat_cols].count()
+        descr.loc[:, 'na%'] = (self.naCount(descr.index)/descr['count'])*100
+        descr.loc[:, 'unique'] = [len(self.df[x].unique()) for x in descr.index]
+        self.cat_descr = descr.T
+
+        return self.cat_descr
+
+    def naCount(self, cols):
+        return self.df[cols].isna().sum(axis = 0)
+    
+    
+
 
 
     
--- tests/test_eda.py
+++ tests/test_eda.py
@@ -5,8 +5,31 @@ sys.path.append('../')
 from py_scripts.eda import Explore
 
 @pytest.mark.explore
-def test_eda(iris_data):
+def test_num_describe(iris_data):
 
     exp = Explore(iris_data)
-    exp.describe()
+    descr = iris_data.describe()
+    num_descr = exp.numDescribe()
+
+    assert num_descr.shape[1] == descr.shape[1]
+    
+    print(num_descr)
+
+@pytest.mark.explore
+def test_cat_describe(iris_data):
+
+    exp = Explore(iris_data)
+    num_descr = exp.numDescribe()
+    cat_descr = exp.catDescribe()
+    num_cols = exp.num_cols
+    cat_cols = exp.cat_cols
+    
+    print(num_descr)
+    print('---')
+    print(cat_descr)
+    
+    assert set(num_cols + cat_cols) == set(iris_data.columns.tolist())
+    assert iris_data.describe().shape[1] == num_descr.shape[1]
+    assert iris_data.shape[1] - iris_data.describe().shape[1] == cat_descr.shape[1]
+    
     
